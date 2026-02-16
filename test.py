0aa54689c03bf80b32ae8effe1311178610a58cf
Anurag-spec1 <anurag13360@gmail.com>
2026-02-16 15:41:06 +0530

    new assignment

--- test.py
+++ test.py
@@ -2,3 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df=pd.read_csv(r"C:\Users\anura\Downloads\income.csv")
+print(df['Designation'].unique())
+
+
+for desig in df['Designation'].unique():
+    mask = df['Designation'] == desig
+    plt.scatter(df.Age[mask],df.Income[mask],label=desig)
+
+plt.xlabel('Age')
+plt.ylabel('Income')
+plt.legend()
