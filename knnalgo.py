ee35cc0ebb7e718535f2d32cf195fa1317360df6
Anurag-spec1 <anurag13360@gmail.com>
2026-02-16 16:07:39 +0530

    knnalgorithm

--- knnalgo.py
+++ knnalgo.py
@@ -1,1 +1,15 @@
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+df=pd.read_csv(r"C:\Users\anura\Downloads\income.csv")
+
+model=KNeighborsClassifier(n_neighbors=3)
+model
+
+x = df.drop(columns=["Designation"])
+y = df["Designation"]
+
+print(x.info())
+print(y.info())    
